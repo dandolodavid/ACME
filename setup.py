ac1910b205d6876f1ef241d34a84c5a44653342f
David <dandolodavid@gmail.com>
2022-12-23 15:49:51 +0100

    label in table class

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='statwolfACME',
-    version='2.0.0.2',    
+    version='2.0.0.3',    
     description='ACME - Accelerated Model Explainability',
     url='https://github.com/dandolodavid/ACME',
     author='Dandolo David',
