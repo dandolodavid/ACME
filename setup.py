2a683afaf0bd1a312373d1238abe7a0a3c743db3
David <dandolodavid@gmail.com>
2023-02-02 16:39:20 +0100

    acme setup update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='statwolfACME',
-    version='2.0.1',    
+    version='2.1.0',    
     description='ACME - Accelerated Model Explainability',
     url='https://github.com/dandolodavid/ACME',
     author='Dandolo David',
